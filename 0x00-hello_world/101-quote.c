b382e82d5a7eb5236f0705eda0b0a3a0ca56e6d2
Eual11 <uchihaeual11@gmail.com>
2022-06-29 16:33:47 -0700

    d

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <unistd>
+/**
+ * main - prints somethinf
+ * Return: 0
+ *
+ */
+int main(void) {
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19",59);
+	return (1);
+}
