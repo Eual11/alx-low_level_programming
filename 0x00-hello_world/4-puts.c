fefea9ab0a522a7458b0747bc697406ee9ec05f3
Eual11 <uchihaeual11@gmail.com>
2022-06-29 15:45:30 -0700

    d

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,6 +5,9 @@
 #include <stdio.h>
 
 int main(void)
+/**
+ * main - prints "Programming is like building a multilngual puzzel,
+ *  followed by a newline return 0*/
 {
 	puts("\"Programming is like building a multilingual puzzle");
 
