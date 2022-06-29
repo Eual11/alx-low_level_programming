d8f17482eac544b7660ee2d4d28f05fcea5189ef
Eual11 <uchihaeual11@gmail.com>
2022-06-29 15:53:47 -0700

    d

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,17 @@
+/*
+ *File: 5-printf.c
+ *Auth: Eual G
+ */
+
+
 #include <stdio.h>
 int main(void)
+	/**
+	 *this program prints with proper grammar, but the outcome is a piece of art
+	 *and a new line 
+	 */
 {
-	printf("with proper grammar, but the outcome is a piece of art, \n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
-	return 0;
+	return (0);
 }
