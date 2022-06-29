cb8f2ef819897c6725d805d24603c3cde7e91e1d
Eual11 <uchihaeual11@gmail.com>
2022-06-29 15:25:56 -0700

    d

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void)
+{
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float))printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	
+
+	return 0;
+}
