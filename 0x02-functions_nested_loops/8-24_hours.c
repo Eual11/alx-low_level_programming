0e051fe632a070902d6e12501dc85a98e76fb457
Eual11 <uchihaeual11@gmail.com>
2022-07-05 14:29:41 -0700

    d

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * jack_bauer - prints days
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int i, j, k, l;
+
+	int max = 9;
+
+	while (l <= 2)
+	{
+		if (l == 2)
+			max = 3;
+
+		for (k = 0; k <= max; ++k)
+		{
+			for (j = 0; j <= 5; ++j)
+			{
+				for (i = 0; i <= 9; ++i)
+				{
+					_putchar(l + '0');
+					_putchar(k + '0');
+					_putchar(':');
+					_putchar(j + '0');
+					_putchar(i + '0');
+					_putchar('\n');
+				}
+			}
+		}
+		l += 1;
+	}
+	return (0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,6 +13,7 @@ int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
 int print_last_digit(int n);
+void jack_bauer(void);
 
 #endif
 
