1af858bbfe6aa1429c6e6e02b4b6b0fe7a84be10
Eual11 <uchihaeual11@gmail.com>
2022-07-05 15:20:04 -0700

    d

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @num1: first number
+ * @num2: second number
+ * Return: num1+num2
+ */
+int add(int num1,int num2)
+{
+	return (num1 + num2);
+}
