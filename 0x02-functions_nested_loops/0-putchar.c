76650aafe0705418c55aca00902532c57e32301d
Eual11 <uchihaeual11@gmail.com>
2022-07-05 05:41:37 -0700

    another day

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+/**
+* File: 0-putchar.c
+* Auth: Eual G
+*/
+#include <stdio.h>
+
+/*
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+
+int main(void)
+{
+	printf("_putchar");
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * _putchar - contains the prototype of all of the functions
+ *@c: the char to print
+ * Return: Always one character to the stream.
+ */
+int _putchar(char c);
+
+#endif
