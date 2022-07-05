0f7558545f079bf5a9062163cf72928b21055b89
Eual11 <uchihaeual11@gmail.com>
2022-07-05 13:50:56 -0700

    f

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *  print_last_digit - print the last digit
+ *  @n: num
+ *  Return: the last digit
+ */
+int  print_last_digit(int n)
+{
+	int last_digit = n % 10;
+
+	return (last_digit);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -12,4 +12,7 @@ int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
 int _abs(int n);
+int print_last_digit(int n);
+
 #endif
+
