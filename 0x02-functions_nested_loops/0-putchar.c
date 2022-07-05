6dac20fd41ab7a42a6c453c228be7d0cc9df95dd
Eual11 <uchihaeual11@gmail.com>
2022-07-05 05:47:08 -0700

    ad

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,13 +3,13 @@
 * Auth: Eual G
 */
 #include <stdio.h>
-
 /**
- * main - check the code
+ * main - writes the characte _putchar to stdo
  *
- * Return: Always 0.
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
-
 
 int main(void)
 {
