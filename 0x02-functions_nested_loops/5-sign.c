97c8c8419598f885c55299d46d3e748380ad6c88
Eual11 <uchihaeual11@gmail.com>
2022-07-05 13:17:47 -0700

    d

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.c"
+#include <unistd.h>
+/**
+ * print_sign - Prints the sign of a num
+ * @n: char
+ * Return: 0 or 1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+')
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0')
+		return (0);
+	}
+
+	else
+		_putchar('-');
+
+	return (-1);
+}
