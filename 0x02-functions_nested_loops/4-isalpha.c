32444b271568e515f6393100e2e1df54fd5e77d3
Eual11 <uchihaeual11@gmail.com>
2022-07-05 12:35:05 -0700

    d

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * _isalpha - checks whethere a char is alphabetical or not
+ * @c: char
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	if (c >= 65 && c <= 90 || c >= 97 && c <= 122)
+		return (1);
+	return (0);
+}
