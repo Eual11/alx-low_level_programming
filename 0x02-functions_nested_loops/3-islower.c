f7bd164ec71d7c2dec43dc8c515ea7893db173f7
Eual11 <uchihaeual11@gmail.com>
2022-07-05 12:10:00 -0700

    a

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checks whethere it is lower case or not;
+ * @c: char
+ * Return: 0 or 1
+ */
+int _islower(int c)
+{
+	if (c >= 97)
+		return (1);
+	return (0);
+}
