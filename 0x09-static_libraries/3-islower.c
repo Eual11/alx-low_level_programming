6200a531aedf63102e063f4905aa20569edd743a
Eual11 <uchihaeual11@gmail.com>
2022-07-27 10:09:04 -0700

    D

---
+++ 0x09-static_libraries/3-islower.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _islower - checks if a string is in lower case
+ * @c: a char
+ * Return: 1 or 0
+ */
+
+int _islower(int c);
---
+++ 0x09-static_libraries/4-isalpha.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if a char is alpha
+ * @c: a char
+ * Returns: 1 or zero
+ */
+
+int _isalpha(int c);
