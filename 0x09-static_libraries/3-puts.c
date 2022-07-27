17997d922e5e5c39eac542d935776c6e5dd088e6
Eual11 <uchihaeual11@gmail.com>
2022-07-27 10:17:12 -0700

    E

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -2,16 +2,7 @@
 
 /**
  * _puts - prints string
- * @str: input string
- * Return: no return
+ * @str: input str * Return: no return
  */
 
-void _puts(char *str)
-{
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
-}
+void _puts(char *str);
